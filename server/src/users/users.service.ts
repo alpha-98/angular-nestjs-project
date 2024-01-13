3a0dccb25509cf5e6a8edc2652611df675e58310
Gambhir Rathore <grathore0110@gmail.com>
2024-01-14 00:11:52 +0530

    rename schemas folder to models

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './schemas/user.schema';
+import { User, UserDocument } from './models/user.schema';
 import { Model } from 'mongoose';
 
 @Injectable()
